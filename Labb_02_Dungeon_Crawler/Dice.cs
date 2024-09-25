bcadfc5c22688fdd87094fc755546c98c865013b
liiinder <linderkristoffer@hotmail.com>
2024-09-25 20:00:53 +0200

    Dice class working

--- Labb_02_Dungeon_Crawler/Dice.cs
+++ Labb_02_Dungeon_Crawler/Dice.cs
@@ -1,5 +1,26 @@
 ﻿class Dice
 {
-    // sides
-    // roll ( amount ) ...
+    private int numberOfDice;
+    private int sidesPerDice;
+    private int modifier;
+    public Dice(int numberOfDice, int sidesPerDice, int modifier)
+    {
+        this.numberOfDice = numberOfDice;
+        this.sidesPerDice = sidesPerDice;
+        this.modifier = modifier;
+    }
+    public override string ToString() => $"{numberOfDice}d{sidesPerDice}+{modifier}";
+    public int Throw()
+    {
+        int sumOfThrows = 0;
+        for (int i = 0; i < numberOfDice; i++)
+        {
+            sumOfThrows += new Random().Next(1, (sidesPerDice + 1));
+        }
+        return sumOfThrows + modifier;
+    }
+    public string Range()
+    {
+        return $"Lowest roll: {numberOfDice + modifier}, Highest roll: {numberOfDice * sidesPerDice + modifier}";
+    }
 }
